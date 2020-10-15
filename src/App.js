996669fc3b8d1afca275e105b47065e0ccf681db
AdibaAbid <55280285+AdibaAbid@users.noreply.github.com>
2020-10-15 11:33:00 +0500

    set style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [list, setList] = useState(["aalo", "piyaz"]);
+  const [list, setList] = useState(["React", "Html"]);
   const [text, setText] = useState("");
-  const [edit, editText] = useState('')
-  const [show, showBtn] = useState(false)
-  const [showAdd, showAddBtn] = useState(true)
+  const [edit, editText] = useState("");
+  const [show, showBtn] = useState(false);
+  const [showAdd, showAddBtn] = useState(true);
 
   const changeText = function (e) {
     const input = e.target.value;
@@ -28,66 +28,96 @@ function App() {
   };
 
   const editItem = function (index) {
-    const newList = [...list]
-    setText(newList[index])
-    showBtn(true)
-    showAddBtn(false)
-    editText(index)
-  }
+    const newList = [...list];
+    setText(newList[index]);
+    showBtn(true);
+    showAddBtn(false);
+    editText(index);
+  };
 
   const update = function () {
-    const newList = [...list]
-    newList.splice(edit, 1, text)
-    setList(newList)
-    setText('')
-    showBtn(false)
-    showAddBtn(true)
-  }
+    const newList = [...list];
+    newList.splice(edit, 1, text);
+    setList(newList);
+    setText("");
+    showBtn(false);
+    showAddBtn(true);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" style={{ width: '16%', marginTop: '-12%' }} />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          style={{ width: "16%", marginTop: "-12%" }}
+        />
         <h1 className="display-3 mt-n4">Todo List</h1>
 
-        <div className="container container-sm">
+        <div className="container container-sm" style={{ width: "60%" }}>
           <div className="input-group mb-3 ">
-            <input type="text" className="form-control" placeholder="Type any item here.."
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Type any item here.."
               onChange={changeText}
-              value={text} aria-label="item text" aria-describedby="button-addon2" />
+              value={text}
+              aria-label="item text"
+              aria-describedby="button-addon2"
+            />
             <div className="input-group-append">
-              {
-                showAdd &&
-                <button className="btn btn-outline-success" onClick={addItem} type="button" id="button-addon2">Add Item </button>
-              }
+              {showAdd && (
+                <button
+                  className="btn btn-outline-success"
+                  onClick={addItem}
+                  type="button"
+                  id="button-addon2"
+                >
+                  Add Item{" "}
+                </button>
+              )}
 
-              {
-                show &&
-                <button className="btn btn-outline-success" onClick={update} type="button" id="button-addon2">Update</button>
-              }
-
+              {show && (
+                <button
+                  className="btn btn-outline-success"
+                  onClick={update}
+                  type="button"
+                  id="button-addon2"
+                >
+                  Update
+                </button>
+              )}
             </div>
           </div>
 
           <ul className="list-group text-dark">
-            {
-              list.map(function (item, index) {
-                return (
-                  <li key={item +"i"} className="list-group-item d-flex justify-content-between text-capitalize">
-                    {item}
-                    <div>
-                      <button className="btn btn-danger mr-2" onClick={() => deleteItem(index)}>Delete</button>
-                      <button className="btn btn-info" onClick={() => editItem(index)}>Edit</button>
-                    </div>
-                  </li>
-
-                );
-              })
-            }
+            {list.map(function (item, index) {
+              return (
+                <li
+                  key={item + "i"}
+                  className="list-group-item d-flex justify-content-between text-capitalize"
+                >
+                  {item}
+                  <div>
+                    <button
+                      className="btn btn-danger mr-2"
+                      onClick={() => deleteItem(index)}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className="btn btn-info"
+                      onClick={() => editItem(index)}
+                    >
+                      Edit
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
           </ul>
-
         </div>
-
       </header>
     </div>
   );
